ecaad070ba8bce2c32d32b7bd936f998a1f3823c
Evan <evanfujita@gmail.com>
2021-01-12 12:15:59 -0600

    almost done

--- app.rb
+++ app.rb
@@ -3,7 +3,36 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      
+      @name = params["pirate"]["name"]
+      @weight = params["pirate"]["weight"]
+      @height = params["pirate"]["height"]
+      @pirate = Pirate.new(@name, @weight, @height)
+      binding.pry
+      @ship1_name = params["pirate"]["ship_name_1"]
+      @ship1_type = params["pirate"]["ships"]["0"]["type"]
+      @ship1_booty = params["pirate"]["ships"]["0"]["booty"]
+      @ship2_name = params["pirate"]["ships"]["1"]["name"]
+      @ship2_type = params["pirate"]["ships"]["1"]["type"]
+      @ship2_booty = params["pirate"]["ships"]["1"]["booty"]
+      @ship1 = Ship.new(@ship1_name, @ship1_type, @ship1_booty)
+      @ship2 = Ship.new(@ship2_name, @ship2_type, @ship2_booty)
+      
+      @pirate.ship1 = ship1
+      @pirate.ship2 = ship2
+      
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height, :ship_1, :ship_2
+
+    @@pirates = []
+
+    def initialize(name, weight, height)
+        @name = name
+        @weight = weight
+        @height = height
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
+
+    def self.clear
+        @@pirates = []
+    end
+
 end
